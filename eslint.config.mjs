7d06fa4f71194f51bac2bc22e738382a30ad5933
Omer Asif Khan <80039162+omerasifkhan@users.noreply.github.com>
2025-09-08 12:53:36 +0500

    feat(packages): updgraded lint and jest related packages

---
+++ eslint.config.mjs
@@ -0,0 +1,112 @@
+// eslint.config.mjs
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths';
+
+export default [
+  {
+    ignores: ['.eslintrc.js', 'dist'],
+
+    files: ['**/*.ts'],
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint,
+      import: importPlugin,
+      prettier,
+      'no-relative-import-paths': noRelativeImportPaths,
+    },
+
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'require-await': 'off',
+      '@typescript-eslint/require-await': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector:
+            'CallExpression[callee.object.name=configService][callee.property.name=/^(get|getOrThrow)$/]:not(:has([arguments.1] Property[key.name=infer][value.value=true])), CallExpression[callee.object.property.name=configService][callee.property.name=/^(get|getOrThrow)$/]:not(:has([arguments.1] Property[key.name=infer][value.value=true]))',
+          message:
+            'Add "{ infer: true }" to configService.get() for correct typechecking. Example: configService.get("database.port", { infer: true })',
+        },
+        {
+          selector:
+            'CallExpression[callee.name=it][arguments.0.value!=/^should/]',
+          message: '"it" should start with "should"',
+        },
+      ],
+
+      'import/no-restricted-paths': [
+        'error',
+        {
+          basePath: './',
+          zones: [
+            {
+              target: ['./src', './test'],
+              from: './dist',
+              message: 'Importing from the `dist` folder is not allowed.',
+            },
+          ],
+        },
+      ],
+
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          pathGroups: [
+            {
+              pattern: '@nest/**',
+              group: 'builtin',
+            },
+            {
+              pattern: '@src/**',
+              group: 'internal',
+            },
+          ],
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['default'],
+        },
+      ],
+
+      'no-relative-import-paths/no-relative-import-paths': [
+        'warn',
+        {
+          allowSameFolder: true,
+          rootDir: 'src',
+          prefix: '@src',
+        },
+      ],
+
+      'prettier/prettier': 'error',
+    },
+  },
+];
